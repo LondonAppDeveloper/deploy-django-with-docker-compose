2e1903d78dd3c5d63b75a864cf08d6c9734740aa
Mark Winterbottom <mark@londonappdeveloper.com>
2021-05-18 14:27:01 +0100

    Added settings.

--- app/app/settings.py
+++ app/app/settings.py
@@ -127,7 +127,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/static/'
+MEDIA_URL = '/static/media/'
+
+MEDIA_ROOT = '/vol/web/media'
+STATIC_ROOT = '/vol/web/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
