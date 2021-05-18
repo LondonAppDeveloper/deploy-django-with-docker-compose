e55572ba434a406c504ef6d406c452729bc3d8b6
Mark Winterbottom <mark@londonappdeveloper.com>
2021-05-18 14:38:17 +0100

    Only add media URLs in debug mode.

--- app/app/urls.py
+++ app/app/urls.py
@@ -20,4 +20,10 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
