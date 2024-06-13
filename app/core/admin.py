692ca1a3bcb321a43cecb1eb6235e08a813d541c
lanan <lvu1@smail.uni-koeln.de>
2024-06-13 11:10:45 +0200

    neue Models erstellt

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
-from core.models import Sample
+from core.models import UserIDList
+from core.models import Answers
 
 
-admin.site.register(Sample)
+admin.site.register(UserIDList)
+admin.site.register(Answers)
+
+
--- app/core/models.py
+++ app/core/models.py
@@ -1,5 +1,19 @@
 from django.db import models
 
 
-class Sample(models.Model):
-    attachment = models.FileField()
+
+
+class UserIDList(models.Model):
+     created_at = models.DateTimeField(auto_now_add=True)
+     updated_at = models.DateTimeField(auto_now=True)
+     userid = models.CharField(max_length=200)    
+def __str__(self):
+         return self.userid
+
+
+class Answers(models.Model):
+    userid = models.ForeignKey(UserIDList, on_delete=models.CASCADE)
+    question_type_id = models.PositiveIntegerField(default=1)
+    data = models.JSONField(default=list,null=True,blank=True)
+    def __str__(self):
+        return self.userid.userid
