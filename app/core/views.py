f237526f5e673b01553de338dae9e8b1d219c6de
lanan <lvu1@smail.uni-koeln.de>
2024-06-17 15:50:41 +0200

    API views erstellt für userid und answers

--- app/core/views.py
+++ app/core/views.py
@@ -6,8 +6,98 @@ from core.models import Answers
 from core.serializers import UserIDListSerializer
 from core.serializers import AnswersSerializer
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
 # Create your views here.
-@csrf_exempt
+
+
+class UserIDListAPIView(APIView):
+
+    def get(self, request):
+        users=UserIDList.objects.all()
+        serializer=UserIDListSerializer(users, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer=UserIDListSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+
+class UserIDListDetailsAPIView(APIView):
+      def get_object(self,id):
+          try: 
+              return UserIDList.objects.get(id=id)
+          except UserIDList.DoesNotExist:
+              return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+
+      def get(self, request, id):
+        id=self.get_object(id)
+        serializer=UserIDListSerializer(id)
+        return Response(serializer.data)   
+      
+      def put(self, request, id):
+          id=self.get_object(id)
+          serializer=UserIDListSerializer(id,data=request.data)
+
+          if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+          return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+      def delete(self, request, id):
+           id=self.get_object(id)
+           id.delete()
+           return Response(status=status.HTTP_204_NO_CONTENT)
+        
+class AnswersAPIView(APIView):
+
+    def get(self, request):
+        answers=Answers.objects.all()
+        serializer=AnswersSerializer(answers, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer=AnswersSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class AnswersDetailsAPIView(APIView):
+    def get_object(self, id):
+        try: 
+            return Answers.objects.get(id=id)
+        except Answers.DoesNotExist:
+            raise Http404
+
+    def get(self, request, id):
+        answer = self.get_object(id)
+        serializer = AnswersSerializer(answer)
+        return Response(serializer.data)   
+      
+    def put(self, request, id):
+        answer = self.get_object(id)
+        serializer = AnswersSerializer(answer, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, id):
+        answer = self.get_object(id)
+        answer.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+@api_view(['GET', 'POST'])
 def useridlist(request):
 
     if request.method=='GET':
@@ -15,19 +105,19 @@ def useridlist(request):
         print(users)  # Debug-Ausgabe der QuerySet-Objekte
         serializer=UserIDListSerializer(users, many=True)
         print(serializer.data)  # Debug-Ausgabe der serialisierten Daten
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
     
     elif request.method=='POST':
-        data=JSONParser().parse(request)
-        serializer=UserIDListSerializer(data=data)
+    
+        serializer=UserIDListSerializer(data=request.data)
 
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=404)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@csrf_exempt   
+@api_view(['GET', 'POST'])  
 def answers(request):
 
     if request.method=='GET':
@@ -35,37 +125,60 @@ def answers(request):
         print(answers)  # Debug-Ausgabe der QuerySet-Objekte
         serializer=AnswersSerializer(answers, many=True)
         print(serializer.data)  # Debug-Ausgabe der serialisierten Daten
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
     
     elif request.method=='POST':
-        data=JSONParser().parse(request)
-        serializer=AnswersSerializer(data=data)
+        
+        serializer=AnswersSerializer(data=request.data)
 
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=404)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 
-@csrf_exempt 
+@api_view(['GET', 'POST', 'DELETE'])  
 def useridlist_detail(request, pk):
     try:
         id=UserIDList.objects.get(pk=pk)
     except UserIDList.DoesNotExist:
-        return HttpResponse(status=404)
+        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
     
     if request.method=='GET':
         serializer=UserIDListSerializer(id)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
     
     elif request.method=='PUT':
-        data=JSONParser().parse(request)
-        serializer=UserIDListSerializer(id,dat=data)
+     
+        serializer=UserIDListSerializer(id,data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
     elif request.method=='DELETE':
         id.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+@api_view(['GET', 'POST', 'DELETE'])  
+def answers_detail(request, pk):
+    try:
+        answer=Answers.objects.get(pk=pk)
+    except Answers.DoesNotExist:
+        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method=='GET':
+        serializer=AnswersSerializer(answer)
+        return Response(serializer.data)
+    
+    elif request.method=='PUT':
+      
+        serializer=AnswersSerializer(answer,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method=='DELETE':
+        answer.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
