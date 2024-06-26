83908377dbc72980efbb0891e2a6f99802094f21
lanan <lvu1@smail.uni-koeln.de>
2024-06-26 11:52:24 +0200

    Objekte in User id lists unter /admin direkt identifizierbar

--- app/core/models.py
+++ app/core/models.py
@@ -34,8 +34,8 @@ class UserIDList(models.Model):
      created_at = models.DateTimeField(auto_now_add=True)
      updated_at = models.DateTimeField(auto_now=True)
      userid = models.CharField(max_length=200)    
-def __str__(self):
-         return self.userid
+     def __str__(self):
+        return self.userid
 
 
 class Answers(models.Model):
